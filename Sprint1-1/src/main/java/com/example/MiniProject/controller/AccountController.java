ec65c9199f1014c40bd947a5c0894307ddadde22
Sasikanth Kanvapuri <sasikanthkanvapuri@gmail.com>
2021-12-13 18:35:02 +0530

    Second Commit

--- Sprint1-1/src/main/java/com/example/MiniProject/controller/AccountController.java
+++ Sprint1-1/src/main/java/com/example/MiniProject/controller/AccountController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+//First Sprint
 
 import com.example.MiniProject.model.Accounts;
 import com.example.MiniProject.model.Logger;
