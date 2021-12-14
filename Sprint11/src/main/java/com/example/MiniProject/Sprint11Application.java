377fba49beb570643f90309d6760141feca102bf
Sasikanth Kanvapuri <'sasikanthkanvapuri@gmail.com>
2021-12-14 10:47:07 +0530

    First Commit

---
+++ Sprint11/src/main/java/com/example/MiniProject/Sprint11Application.java
@@ -0,0 +1,14 @@
+package com.example.MiniProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//First
+public class Sprint11Application{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint11Application.class, args);
+	}
+
+}
---
+++ Sprint11/src/main/java/com/example/MiniProject/model/Accounts.java
@@ -0,0 +1,50 @@
+package com.example.MiniProject.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Accounts")
+public class Accounts {
+	@Id
+	private int acctID;
+	private int balance;
+	private String acctStatus;
+
+	public Accounts() {
+
+	}
+
+	public Accounts(int acctID, int balance, String acctStatus) {
+		super();
+		this.acctID = acctID;
+		this.balance = balance;
+		this.acctStatus = acctStatus;
+	}
+
+	public int getAcctID() {
+		return acctID;
+	}
+
+	public void setAcctID(int acctID) {
+		this.acctID = acctID;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public String getAcctStatus() {
+		return acctStatus;
+	}
+
+	public void setAcctStatus(String acctStatus) {
+		this.acctStatus = acctStatus;
+	}
+
+}
